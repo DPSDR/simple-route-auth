5479864d83498e6078497b6aadace0302a217a8d
DPSDR <dipta751@gmail.com>
2022-09-25 23:28:23 +0600

    sign in with google done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase.';
 
 const Login = () => {
+    const { sighInWithGoogle } = useFirebase()
+
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{ margin: '20px' }}>
+                <button onClick={sighInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" placeholder='Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
